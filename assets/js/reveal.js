16d17bb7036f668fb437450f799b2f08cfbebe58
imneli <contatodoneli@gmail.com>
2024-07-03 21:57:14 -0300

    Commit 6: Scroll Reveal at the Home

---
+++ assets/js/reveal.js
@@ -0,0 +1,12 @@
+window.revelar = ScrollReveal({ reset:true })
+
+revelar.reveal('.container-img', {
+    duration: 2000,
+    distance: '90px',
+    delay: 250
+})
+
+revelar.reveal('.container-oriation', {
+    duration: 2000,
+    distance: '90px',
+})
