941b09dcafb284124361f5c81f1925beec2ca285
imneli <contatodoneli@gmail.com>
2024-07-05 10:10:32 -0300

    Commit 8: Section 3

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -23,7 +23,7 @@ revelar.reveal('.curso1', {
 revelar.reveal('.curso2', {
     duration: 2000,
     distance: '-120px',
-    delay: 250
+    delay: 250,
 })
 revelar.reveal('.curso3', {
     duration: 2000,
@@ -34,4 +34,22 @@ revelar.reveal('.curso3', {
 revelar.reveal('.seta2', {
     duration: 2000,
     distance: '-120px',
+})
+
+revelar.reveal('.price1', {
+    duration: 2000,
+    distance: '-120px',
+    origin: 'right'
+})
+revelar.reveal('.price2', {
+    duration: 2000,
+    distance: '-120px',
+    delay: 500,
+    origin: 'right'
+})
+revelar.reveal('.price3', {
+    duration: 2000,
+    distance: '-120px',
+    delay: 750,
+    origin: 'right'
 })
