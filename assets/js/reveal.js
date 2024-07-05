34c75521966b473106a64c4f0390fe4271675864
imneli <contatodoneli@gmail.com>
2024-07-05 09:27:22 -0300

    Commit 7: Section 2 (Courses)

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -10,3 +10,28 @@ revelar.reveal('.container-oriation', {
     duration: 2000,
     distance: '90px',
 })
+
+revelar.reveal('.seta1', {
+    duration: 2000,
+    distance: '-120px',
+})
+
+revelar.reveal('.curso1', {
+    duration: 2000,
+    distance: '-120px',
+})
+revelar.reveal('.curso2', {
+    duration: 2000,
+    distance: '-120px',
+    delay: 250
+})
+revelar.reveal('.curso3', {
+    duration: 2000,
+    distance: '-120px',
+    delay: 500
+})
+
+revelar.reveal('.seta2', {
+    duration: 2000,
+    distance: '-120px',
+})
